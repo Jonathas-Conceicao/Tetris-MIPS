29bd39fd4e7c413aff2646ec94cd053e730a93a5
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2016-06-18 19:04:28 -0300

    Macrosgit add Main.asm  Macrosgit add Main.asm  Macrosgit add Main.asm

--- Main.asm
+++ Main.asm
@@ -1,6 +1,7 @@
 ######################
 # Macros para a Pilha#
 ######################
+
 .macro pushWord (%dado)
 	addi $sp $sp -4
 	sw %dado ($sp)
@@ -8,20 +9,46 @@
 .macro popWord (%dado)
 	lw %dado ($sp)
 	addi $sp $sp 4
-.end_macro	
+.end_macro
+
+############################
+#Macros para mover ponteiro#
+############################
+
+#Moves the pointer to the next 'n' square horizontalçy
+.macro nextSquareHorizontal (%ponteiro, %range) #$1: Memori Poniter; $2: Quantity of Squares to jump
+	pushWord $t0
+	and $t0 $0 $0
+loop:
+	addi %ponteiro %ponteiro 64 # 16*6
+	addi $t0 $t0 1
+	blt $t0 %range loop
+	nop
+.end_macro
+
+#Moves the pointer to the next 'n' square vertically
+.macro nextSquareVertical (%ponteiro, %range) #$1: Memori Poniter; $2: Quantity of Squares to jump
+	pushWord $t0
+	and $t0 $0 $0
+loop:
+	addi %ponteiro %ponteiro 32768 #64 * 32 * 16
+	addi $t0 $t0 1
+	blt $t0 %range loop
+	nop
+.end_macro
 
 ################################
 # Macros para a printar na tela#
 ################################
 
-.macro paintSquare (%cor, %ponteiro, %flag) #$1 Color to paint; $2: start pixel pointer; $3 0 returns origal pointer, any other value dont; 
+#Paint with ($2) color the square that starts and pointer($2)
+.macro paintSquare (%cor, %ponteiro, %flag) #$1: Color to paint; $2: square pointer; $3: '0' returns origal pointer, else returns the finish pointer;
 	pushWord $t0
 	pushWord $t1
 	pushWord %ponteiro
 	and $t0 $zero $zero
 	and $t1 $zero $zero
-
-loopLinha:	
+loopLinha:
 	sw %cor, (%ponteiro)
 	addi %ponteiro %ponteiro 4 #Next Pixel
 	addi $t0 $t0 1 #Line counter increment
@@ -34,11 +61,11 @@ loopLinha:
 	nop
 	beq $zero %flag StartPointer
 	nop
-	popWord $t1 #Descarda a posição antiga do ponteiro 
+	popWord $t1 #Descarda a posição antiga do ponteiro
 	popWord $t1
 	popWord $t0
 	j return
-	nop	
+	nop
 StartPointer:
 	popWord %ponteiro #Poniter returns at start position
 	popWord $t1
@@ -46,53 +73,88 @@ StartPointer:
 return:
 .end_macro
 
-
-.macro paintFullLine (%cor, %ponteiro, %flag)
+#Paint a full squareline
+.macro paintFullLine (%cor, %ponteiro, %flag) #$1: Color to paint; $2: square pointer; $3: '0' returns origal pointer, else returns the finish pointer;
 	pushWord $t0
 	pushWord %ponteiro
 	and $t0 $zero $zero
 loopLinha:
 	sw %cor (%ponteiro)
-	addi %ponteiro %ponteiro 4 #Next Pixel 
+	addi %ponteiro %ponteiro 4 #Next Pixel
 	add $t0 $t0 1
 	blt $t0 8192 loopLinha #8192 (512*16)
 	nop
 	beq $zero %flag StartPointer
 	nop
-	popWord $t0 #Descarda a posição antiga do ponteiro 
+	popWord $t0 #Descarda a posição antiga do ponteiro
 	popWord $t0
 	j return
-	nop	
+	nop
 StartPointer:
 	popWord %ponteiro #Poniter returns at start position
 	popWord $t0
 return:
 .end_macro
 
-.macro paintColumn (%cor, %ponteiro, %quant, %flag)
-	pushWord $t7
+#Paint a line of 'n' squares
+.macro paintLine (%cor, %ponteiro, %range, %flag)#$1 Color to paint; $2: start pixel pointer; $3 Number of squares to paint; $4: '0' returns origal pointer, else returns the finish pointer;
+	pushWord $t0
 	pushWord %ponteiro
-loopColuna:
-	paintSquare %cor, %ponteiro, 1
-	addi $t7 $t7 1
-	blt $t7 %quant loopColuna
+	and $t0 $zero $zero
+loopLine:
+	paintSquare %cor, %ponteiro, 0
+	addi $t0 $t0 1
+	addi %ponteiro %ponteiro 64 #Next block (16*4)
+	blt $t0 %range loopLine
 	nop
 	beq $zero %flag StartPointer
 	nop
-	popWord $t7 #Descarda a posição antiga do ponteiro 
-	popWord $t7
+	popWord $t0 #Descarda a posição antiga do ponteiro
+	popWord $t0
 	j return
-	nop	
+	nop
 StartPointer:
 	popWord %ponteiro #Poniter returns at start position
-	popWord $t7
+	popWord $t0
 return:
 .end_macro
 
-.text 
-printInterface:
+#Paint a column of 'n' squares
+.macro paintColumn (%cor, %ponteiro, %range, %flag)#$1 Color to paint; $2: square pointer; $3 Number of squares to paint; $4: '0' returns origal pointer, else returns the finish pointer;
+	pushWord $t0
+	pushWord %ponteiro
+loopColuna:
+	paintSquare %cor, %ponteiro, 1
+	addi $t0 $t0 1
+	blt $t0 %range loopColuna
+	nop
+	beq $zero %flag StartPointer
+	nop
+	popWord $t0 #Descarda a posição antiga do ponteiro
+	popWord $t0
+	j return
+	nop
+StartPointer:
+	popWord %ponteiro #Poniter returns at start position
+	popWord $t0
+return:
+.end_macro
+
+.text
+printBaseInterface:
 	la $s0 0x797979
 	and $s1 $gp $gp
 	#generateBorder($t3, 0, 5632, $t0, $t1)
-	paintFullLine $s0 $s1 1
-	paintColumn $s0 $s1 31 0
+	paintFullLine $s0 $s1 0
+	la $s0 0x71e392
+	nextSquareVertical $s1 1
+	paintLine $s0 $s1 16 0
+	nextSquareHorizontal $s1 16
+	la $s0 0x6110a2
+	paintLine $s0 $s1 16 0
+	#paintColumn $s0 $s1 32 0
+	#paintSquare $s0 $s1 0
+	#nextSquareHorizontal $s1
+	#paintSquare $s0 $s1 0
+	#nextSquareVertical $s1
+	#paintSquare $s0 $s1 0
